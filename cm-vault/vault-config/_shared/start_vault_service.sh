a9054dba1029ec98ff1e1f037f9bc11e7a1e5dcb
lonelyCZ <531187475@qq.com>
2022-07-14 21:04:31 +0800

    Add logs output for Vault port-forwarding
    Signed-off-by: lonelyCZ <531187475@qq.com>

--- cm-vault/vault-config/_shared/start_vault_service.sh
+++ cm-vault/vault-config/_shared/start_vault_service.sh
@@ -8,9 +8,10 @@ SERVICE="service/vault"
 NAMESPACE="vault"
 
 echo "Executing 'kubectl port-forward' to establish the connection to Vault installed in Kubernetes"
+echo "You can check the port-forward logs at 'vault-config/port-forward.log'"
 
-echo -en "kubectl port-forward $SERVICE $NODE_PORT:$CONTAINER_PORT -n $NAMESPACE\n disown" > run_service.sh && chmod +x run_service.sh
-nohup bash run_service.sh </dev/null >/dev/null 2>&1 &
+echo -en "kubectl port-forward $SERVICE $NODE_PORT:$CONTAINER_PORT -n $NAMESPACE" > run_service.sh && chmod +x run_service.sh
+nohup bash run_service.sh </dev/null >port-forward.log 2>&1 &
 
 # sleep was to give it a chance to finish establishing the connection before terragrunt/terraform starts running
 echo "Sleeping for 10s while waiting for connection to be established"
